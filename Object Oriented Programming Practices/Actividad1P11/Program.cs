da94e1a472430e983ae592d8f1b6382c9620aacd
Luis Mario Recinos Hernandez <92188399+Mare1702@users.noreply.github.com>
2023-05-28 21:29:51 -0600

    Add files via upload

---
+++ Object Oriented Programming Practices/Actividad1P11/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Actividad1P11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opc,opcMenu, Intentos = 1, Score=0, k=0;
+            String Nombre;
+            String[] Puntajes = new string[150];
+            Console.WriteLine("Bienvenid@ al juego de las adivinanzas");
+            do
+            {
+                Console.WriteLine("¿Qué desea hacer?\n1. Jugar\t2. Ver puntajes\t3. Salir");
+                opcMenu = Convert.ToInt32(Console.ReadLine());
+                switch (opcMenu)
+                {
+                    case 1:
+                        Intentos = 1;
+                        Console.WriteLine("Hay un solo número ganador entre un conjunto del 1 al 30");
+                        Console.WriteLine("¡Intente adivinar el correcto!");
+                        do
+                        {
+                            Console.Write("Intento " + Intentos);
+                            Console.WriteLine(" ");
+                            opc = Convert.ToInt32(Console.ReadLine());
+                            if (opc != 19)
+                            {
+                                Console.WriteLine("¡Respuesta incorrecta!");
+                                Intentos++;
+                            }
+                        } while (opc != 19);
+                        Console.WriteLine("¡Acertaste!");
+                        Console.WriteLine("Registre su nombre con su puntaje");
+                        Nombre = Console.ReadLine();
+                        if (Intentos < 6)
+                            Score = 100;
+                        else if (Intentos > 5 && Intentos < 16)
+                            Score = 100 - 35;
+                        else if (Intentos > 15 && Intentos < 27)
+                            Score = 100 - 50;
+                        else if (Intentos > 26)
+                            Score = 100 - 100;
+
+                        if (Intentos == 1)
+                            Score = Score + 150;
+                        else if (Intentos > 1 && Intentos < 6)
+                            Score = Score + 10;
+                        else if (Intentos > 5 && Intentos < 16)
+                            Score = Score + 4;
+                        else if (Intentos > 15 && Intentos < 27)
+                            Score = Score + 2;
+                        else if (Intentos > 26)
+                            Score = Score + 1;
+                        Console.WriteLine("Usuario:  Nombre: " + Nombre + " | Intentos Requeridos: " + Intentos + " | Puntaje: " + Score);
+                        Puntajes[k] = ("Nombre: "+Nombre+ " | Intentos: "+Intentos+" | Puntaje: "+Score);
+                        k++;
+                        break;
+                    case 2:
+                       
+                        Console.WriteLine("Los puntajes obtenidos son: ");
+                        StreamWriter punt = File.CreateText("Puntajes");
+                        for (int i = 0; i < k + 1; i++)
+                        {
+                            punt.WriteLine(Puntajes[i]);
+                            Console.WriteLine(Puntajes[i]);
+                        }
+                        punt.Close();
+                        break;
+                    case 3:
+                        Console.WriteLine("¡Gracias por jugar!");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida, por favor ingrese una correcta");
+                        break;
+                }
+            } while (opcMenu != 3);
+        }
+    }
+}
---
+++ Object Oriented Programming Practices/Actividad3Practicaa3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace actividadnumerosaleatorios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Tiradas = new int[100];
+            int i, elBueno, Repeticiones = 0;
+            Console.WriteLine("¡¡Observación de repeticiones en el lanzamiento de un dado!!\n\n");
+            Console.WriteLine("¡Presiona Enter para lanzar los dados!\n\n");
+            Console.Read();
+            Random aleatorio = new Random();
+            for (i = 0; i < Tiradas.Length; i++)
+            {
+                Tiradas[i] = aleatorio.Next(1, 6);
+                Console.WriteLine("{0}\t", Tiradas[i]) ;
+            }
+            
+            Console.WriteLine("¡¡¡El primer número sacado en los dados fue: {0}!!!", Tiradas[0]);
+            elBueno = Tiradas[0];
+            for (i = 0; i < Tiradas.Length; i++)
+            {
+                if (Tiradas[i] == elBueno)
+                {
+                    Repeticiones++;
+                }
+            }
+            Console.WriteLine("¡¡¡Las veces que se repitió el {0} entre las 100 tiradas del dado fueron {1}!!!", elBueno, Repeticiones);
+            Console.Read();
+
+        }
+    }
+}
